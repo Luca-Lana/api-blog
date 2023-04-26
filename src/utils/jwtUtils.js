275df8167a869ea1ae779dc09089c7021787dc9f
Luca <lucalana13@gmail.com>
2023-04-26 20:12:49 -0300

    removendo async das funções

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken')
 const jwtConfig = ('../config/jwtConfig')
 
 module.exports = {
-	async criarToken(objeto) {
+	criarToken(objeto) {
 		return jwt.sign(objeto, jwtConfig.chave, { expiresIn: jwtConfig.expira_em })
 	},
 
-	async validarToken(token) {
+	validarToken(token) {
 		try {
 			return jwt.verify(token, jwtConfig.chave)
 		}catch (erro) {
