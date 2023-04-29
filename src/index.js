a0278f18f04dc97ccb96b68335ba7fc4c9dcb868
Luca <lucalana13@gmail.com>
2023-04-29 15:13:10 -0300

    add rotas de publicao ao arquivo principal

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 require('./config/databaseConfig.js')
 const autenticacaoRoutes = require('./routes/autenticacaoRoutes')
+const publicacaoRoutes = require('./routes/publicacaoRoutes')
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(autenticacaoRoutes)
+
+app.use(publicacaoRoutes)
 
 app.listen(8080, () => {
 	console.log('Servidor está rodando')
