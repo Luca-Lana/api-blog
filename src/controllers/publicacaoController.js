a1e48527aa93020081f5e1e325d5761ea01f13a9
Luca <lucalana13@gmail.com>
2023-04-29 20:05:21 -0300

    criando função de busca post pelo id

--- src/controllers/publicacaoController.js
+++ src/controllers/publicacaoController.js
@@ -1,4 +1,5 @@
 const publicacaoModel = require('../models/publicacaoModel')
+const comentarioModel = require('../models/comentarioModel')
 
 module.exports = {
 
@@ -9,6 +10,23 @@ module.exports = {
 		} catch (erro) {
 			console.error(erro)
 			res.status(400).json({msg: 'Erro ao acessar as publicações'})
+		}
+	},
+
+	async obterPublicacaoId (req, res) {
+		try {
+			const { id:idPublicacao } = req.params
+			let resultadoPublicacao = await publicacaoModel.findOne({_id: idPublicacao})
+																				.select(['autor', 'titulo', 'conteudo', 'criado_em', 'atualizado_em'])
+			let resultadoComentarios = await comentarioModel.find({post: resultadoPublicacao._id})
+																				.select(['autor', 'conteudo', 'criado_em', 'atualizado_em'])
+			res.status(200).json({publicacao: resultadoPublicacao, comentarios: resultadoComentarios})
+		} catch (erro) {
+			if (erro.message.includes('Cast to ObjectId')) {
+				res.status(400).json({msg: 'Publicação não existe'})
+			} else {
+				res.status(400).json({msg: 'Erro ao acessar publicacão'})
+			}
 		}
 	},
 
