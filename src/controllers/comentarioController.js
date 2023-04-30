9bbf8eebb08d976a485cefabc6bc9fdbdaffb789
Luca <lucalana13@gmail.com>
2023-04-30 17:45:25 -0300

    criando controle comentario

--- src/controllers/comentarioController.js
+++ src/controllers/comentarioController.js
@@ -5,11 +5,11 @@ module.exports = {
 
 	async criarComentario (req, res) {
 		try {
-			const { id:idPublicacao } = req.params
-			const { conteudo } = req.body
-			let { autor } = req.headers		
+			let { id:idPublicacao } = req.params
+			let { conteudo } = req.body
+			let { idAutor } = req.headers		
 			let publicacao = await publicacaoModel.findOne({_id: idPublicacao})
-			await comentarioModel.create({idAutor: autor, idPublicacao, conteudo})
+			await comentarioModel.create({idAutor, idPublicacao, conteudo})
 			res.status(200).json({msg: 'Cometário feito com sucesso'})
 		} catch (erro) {
 			res.status(400).json({msg: 'Houve um problema ao comentar'})
@@ -17,6 +17,12 @@ module.exports = {
 	},
 
 	async removerComentario (req, res) {
-
+		try {
+			let { id } = req.params
+			await comentarioModel.deleteOne({_id: id})
+			res.status(200).json({msg: 'Comentario excluido com sucesso'})
+		} catch (erro) {
+			res.status(400).json({msg: 'Problema ao excluir comentario'})
+		}
 	}
 }
